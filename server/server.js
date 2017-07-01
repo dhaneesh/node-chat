e9a759313231accfbe3632e65843f9b212802050
dhaneesh <dhaneesh88@gmail.com>
2017-07-01 17:24:22 +0530

    utilis

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const {generateMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3010;
@@ -14,14 +15,12 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'John',
-    text: 'See you then',
-    createdAt: 123123
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat application'));
+  socket.broadcast.emit('newMessage',generateMessage('Admin', 'New user joined'));
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage',generateMessage(message.from, message.text));
   });
 
   socket.on('disconnect', () => {
